0923041192edc3b8690357da9c667437a447524c
Leonardo-Maxcell <i1821336@continental.edu.pe>
2022-04-27 12:45:08 -0500

    second commit

--- Origen/content/content.js
+++ Origen/content/content.js
@@ -2,6 +2,8 @@ var a = 2;
 var b = 2;
 resultado = (a + b) -100
 
-
+//segundo cambio
+resultado = resultado + b
 
 console.log(resultado)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Hola este es el primer cambio 
 que realizare.
+
+Hola este es el segundo cambio que guardare
